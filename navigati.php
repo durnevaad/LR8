2e408c8ffaa3f81dd7d5d5255e30d3a7aab73956
durnevaad <93126385+durnevaad@users.noreply.github.com>
2021-11-05 14:47:51 +0500

    прописала пути для сайта

---
+++ navigati.php
@@ -0,0 +1,12 @@
+<title>Дурнева А.Д.</title>
+<ul>Дурнева А.Д. ЭАС-310</ul>
+<ul>Задания:</ul>
+<li><a href="lab8.html">Упражнение 1-4</a></li>
+<li><a href="lab_9z_1.html">Задание 1</a></li>
+<li><a href="lab_9z_2.html">Задание 2</a></li>
+<li><a href="lab_9z_3.html">Задание 3</a></li>
+<li><a href="lab_9z_4.html">Задание 4</a></li>
+<li><a href="lab_9z_5.html">Задание 5</a></li>
+<li><a href="lab_9z_6.html">Задание 6</a></li>
+<li><a href="lab_9z_7.html">Задание 7</a></li>
+<p><a href="../index.php">Вернуться назад</a></p>
